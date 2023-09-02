8acc89739dfab6f7b2c935adbcf98dbd60c19073
Garry Lai <garrylai0306@gmail.com>
2023-09-02 12:55:14 +0800

    新增 雷達

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const g = svg.append("g");
 
 const county_map_url = './COUNTY_MOI_1090820.json';
 const town_map_url = './TOWN_MOI_1120317.json';
+
 const temp_url = 'https://cwbopendata.s3.ap-northeast-1.amazonaws.com/DIV2/O-A0038-003.json';
 const rain_url = 'https://cwbopendata.s3.ap-northeast-1.amazonaws.com/DIV2/O-A0040-004.json';
-//const qpesums_url = 'https://cwbopendata.s3.ap-northeast-1.amazonaws.com/MSC/O-B0045-001.json';
+const qpesums_rain_url = 'https://cwbopendata.s3.ap-northeast-1.amazonaws.com/MSC/O-B0045-001.json';
+const qpesums_radar_url = 'https://cwbopendata.s3.ap-northeast-1.amazonaws.com/MSC/O-A0059-001.json';
 
 svg.call(d3.zoom().on("zoom",() => {
 	g.attr("transform", d3.event.transform);
@@ -25,24 +27,24 @@ function print(...data) {
 	console.log(data);
 }
 
-function data_proc(data, nan_value) {
+function data_proc(data, nan_value, fix=0, offset=0) {
 	data_out = [];
 	
 	parameter = data['cwbopendata']['dataset']['datasetInfo']['parameterSet']['parameter'];
 	
-	lon0_lat0 = parameter[0]['parameterValue'].split(',');
+	lon0_lat0 = parameter[0+offset]['parameterValue'].split(',');
 	lon0 = parseFloat(lon0_lat0[0]);
 	lat0 = parseFloat(lon0_lat0[1]);
 	
-	dx = parseFloat(parameter[1]['parameterValue']);
+	dx = parseFloat(parameter[1+offset]['parameterValue']);
 	
-	valid_time = parameter[2]['parameterValue'];
+	valid_time = parameter[2+offset]['parameterValue'];
 	
-	nx_ny = parameter[3]['parameterValue'].split('*');
-	nx = parseInt(nx_ny[0], 10);
+	nx_ny = parameter[3+offset]['parameterValue'].split('*');
+	nx = parseInt(nx_ny[0], 10)+fix;
 	ny = parseInt(nx_ny[1], 10);
 	
-	unit = parameter[4]['parameterValue'].replace('攝氏', '℃');
+	unit = parameter[4+offset]['parameterValue'].replace('攝氏', '℃');
 	
 	data_content = data['cwbopendata']['dataset']['contents']['content'].split(',');
 	x = 0;
@@ -59,12 +61,13 @@ function data_proc(data, nan_value) {
 				'lon': lon.toFixed(2),
 				'lat': lat.toFixed(2),
 				'data': parseFloat(value),
+				'size': dx*200,
 				'unit': unit,
 			});
 		}
 		
 		x++;
-		if (x >= nx-1) {
+		if (x >= nx) {
 			x = 0;
 			y++;
 		}
@@ -117,21 +120,29 @@ async function draw_map() {
 		})
 }
 
-async function draw(option='溫度') {
+async function draw(option='溫度GT') {
 	d3.selectAll("rect").remove();
 	
-	[temp_data, rain_data] = await Promise.all([
+	[temp_data, rain_data, qpesums_rain_data, qpesums_radar_data] = await Promise.all([
 		d3.json(temp_url),
 		d3.json(rain_url),
+		d3.json(qpesums_rain_url),
+		d3.json(qpesums_radar_url),
 	])
 	
 	//Temp Data
-	if (option == '雨量') {
-		data = data_proc(rain_data, -1);
+	if (option == '雨量GT') {
+		data = data_proc(rain_data, -1, -1);
 		cb = raincb;
-	} else if (option == '溫度') {
-		data = data_proc(temp_data, -999);
+	} else if (option == '溫度GT') {
+		data = data_proc(temp_data, -999, -1);
 		cb = tempcb;
+	} else if (option == 'QPESUMS雨量') {
+		data = data_proc(qpesums_rain_data, -1);
+		cb = raincb;
+	} else if (option == 'QPESUMS回波') {
+		data = data_proc(qpesums_radar_data, -999, 0, 1);
+		cb = radarcb;
 	}
 	
 	g.selectAll("circle")
@@ -144,8 +155,8 @@ async function draw(option='溫度') {
 		.attr("y", function(d) {
 			return d['cy'];
 		})
-		.attr('width', 6)
-		.attr('height', 6.5)
+		.attr('width', function(d) {return d['size']})
+		.attr('height', function(d) {return d['size']+1})
 		.style('fill', function(d) {return cmap(cb, d['data'])})
 		.on("mouseover", function(d) {
 			d3.select('#tooltip').style('opacity', 1).html('<div class="custom_tooltip">' + d['lon'] + ', ' + d['lat'] + '<br>' + d['data'] + ' ' + d['unit'] + '</div>')
